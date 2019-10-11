cf5f89479d2c8a2bdc3cc310ad0684923c34c37d
tomek <tomasz.kozielski@mksiegowa.pl>
2019-10-11 15:21:55 +0200

    First 2 options

--- src/App.js
+++ src/App.js
@@ -1,24 +1,106 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import styled from 'styled-components';
+
+const Table = styled.table`
+  border: 1px solid black;
+
+  td {
+    background: grey;
+  },
+  th, td {
+    padding: 5px;
+  }
+  
+`;
+
+function get(i) {
+
+  if (i==0)
+    return 0
+  if (i==1)
+    return 1
+
+  return (get(i-1)+get(i-2))
+}
+
+function fetchObject() {
+
+  let count = -1
+  let prev = 0
+  let next = 1
+  let temp = 0
+
+  return {
+    set: i => { 
+      count++
+      if (count==0)
+        return 0
+      else if (count==1)
+        return 1
+      else {
+        temp = next
+        next = next + prev
+        prev = temp
+        return next
+      }
+    },
+    prev: ()  => prev, 
+    next: ()  => next, 
+  }
+}
 
 function App() {
+
+  let d = 0;
+  const data = fetchObject();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h3>Recurrency</h3>
+    <Table>
+    <tr>
+    <th>Index</th>
+    <th>Result</th>
+    </tr>
+    { [0,1,2,3,4,5,6,7,8,9,10,11,12].map(k => {
+      d = get(k)
+      return (
+        <tr>
+        <td>index:{k}</td>
+        <td>{d}</td>
+        </tr>
+      )
+    }
+    )}
+    </Table>
+    </div>
+
+    <div> 
+    <h3>two variables</h3>
+    <Table>
+    <tr>
+    <th>Index</th>
+    <th>Result</th>
+    <th>Parameters</th>
+    </tr>
+    { [0,1,2,3,4,5,6,7,8,9,10,11,12].map(k => {
+      let prev = data.prev()
+      let next = data.next()
+      let ret = data.set()
+      return(
+        <tr>
+        <td>index:{k}</td>
+        <td style={{margin:'20px'}}>{ret}</td>
+        <td>prev:{prev} next:{next}</td>
+        </tr>
+      )
+    })
+    }
+    </Table>
+
+    </div>
     </div>
   );
 }
